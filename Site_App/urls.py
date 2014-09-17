c09cc16ff8f0d6aadbf8481cc1555fa2bbae50f3
Jonathan <jcoop565@gmail.com>
2014-09-17 16:25:07 -0400

    Site header, static and templates

--- Site_App/urls.py
+++ Site_App/urls.py
@@ -3,6 +3,5 @@ from django.conf.urls import patterns, url
 from Site_App import views
 
 urlpatterns = patterns('',
-	url(r'^$', views.index, name='index'),
-	url(r'^test/', views.test, name='test')
+	url(r'^$', views.index, name='index')
 )
--- Site_App/views.py
+++ Site_App/views.py
@@ -2,7 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the site index.")
-	
-def test(request):
-    return HttpResponse("TEST")
+    return render(request, 'index.html')
