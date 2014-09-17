274be4e637a8eb65ead341757865770bf65b8240
Jonathan <jcoop565@gmail.com>
2014-09-17 14:12:31 -0400

    Added site app, folder tree

--- Senior_Project/urls.py
+++ Senior_Project/urls.py
@@ -4,9 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'Senior_Project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+	url(r'^$', include('Site_App.urls')),
+	url(r'^site/', include('Site_App.urls')),
 )
