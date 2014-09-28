4a5850ecb74e47220bd177fa502bdcb8074aa428
Jonathan <jcoop565@gmail.com>
2014-09-28 16:20:37 -0400

    Added basic site structure, temporary navigation

--- Site_App/views.py
+++ Site_App/views.py
@@ -3,3 +3,15 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+	
+def login(request):
+    return render(request, 'login.html')
+	
+def register(request):
+    return render(request, 'register.html')
+	
+def profile(request):
+    return render(request, 'profile.html')
+	
+def search(request):
+    return render(request, 'search.html')
